4083b098896daeba14dca863581ba2e17d36be71
Aniket Raj <75392672+aniketraz@users.noreply.github.com>
2024-01-10 16:34:33 +0530

    Time: 0 ms (100.00%), Space: 9.4 MB (40.86%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -1,7 +1,7 @@
 class Solution {
  public:
   void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-    int i = m - 1;      // nums1's index (the actual nums)
+    int i = m - 1;
     int j = n - 1;      // nums2's index
     int k = m + n - 1;  // nums1's index (the next filled position)
 
